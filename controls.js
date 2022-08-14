d007c925f810defea9a2dd782b79892001dab124
Salar mokhtarilaleh <salarmokhtari0@gmail.com>
2022-08-14 23:32:20 +0430

    Main

--- controls.js
+++ controls.js
@@ -9,5 +9,40 @@ class Controls{
         this.#addKeyboardListeners();
     }
 
-    #
+    #addKeyboardListeners(){
+         document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
+                    break;
+                case "ArrowRight":
+                    this.right=true;
+                    break;
+                case "ArrowUp":
+                    this.forward=true;
+                    break;
+                case "ArrowDown":
+                    this.reverse=true;
+                    break;
+            }
+            console.table(this);
+         }
+        document.onkeyup=(event)=>{
+           switch(event.key){
+               case "ArrowLeft":
+                   this.left=false;
+                   break;
+               case "ArrowRight":
+                   this.right=false;
+                   break;
+               case "ArrowUp":
+                   this.forward=false;
+                   break;
+               case "ArrowDown":
+                   this.reverse=false;
+                   break;
+           }
+           console.table(this);
+        }
+    }
 }
